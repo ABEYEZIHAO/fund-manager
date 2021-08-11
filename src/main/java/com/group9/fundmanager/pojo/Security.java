4a5d1492c43e042a20ff3d369fa1e8ab8cc335c4
Ritaye <rita-0913@hotmail.com>
2021-08-11 11:52:25 +0800

    Update Security.java

--- src/main/java/com/group9/fundmanager/pojo/Security.java
+++ src/main/java/com/group9/fundmanager/pojo/Security.java
@@ -1,4 +1,42 @@
-package com.group9.fundmanager.pojo;
+package com.dennis.springboot.pojo;
 
-public class Security {
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Securities")
+public class Securities
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "s_id")
+    private long s_id;
+
+    @Column(name = "symbol")
+    private String symbol;
+
+
+    public long s_id() {
+        return s_id;
+    }
+    public void setS_id(long s_id) {
+        this.s_id = s_id;
+    }
+    public long getS_id() {
+        return s_id;
+    }
+    public String getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
 }
