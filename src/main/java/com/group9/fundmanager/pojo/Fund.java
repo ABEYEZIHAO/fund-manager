309629071ea8b3ccd4d40a9eccd6d777fa2d21a4
Abe <934982126@qq.com>
2021-08-11 13:30:03 +0800

    Update Fund.java

--- src/main/java/com/group9/fundmanager/pojo/Fund.java
+++ src/main/java/com/group9/fundmanager/pojo/Fund.java
@@ -1,43 +1,52 @@
 package com.group9.fundmanager.pojo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.sql.ClientInfoStatus;
+import java.util.List;
 
 @Entity
 @Table(name = "funds")
 public class Fund {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    @Column(name = "id") 
-	private Long id;
-    
-    @Column(name = "name")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
 	private String name;
 
-	public Fund() {
-	}
+	@Column(name = "m_id")
+	private long m_id;
 
-	public Fund(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+	@Column(name = "positions")
+	private List<Long> positions;
 
-	public Long getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
+
 		this.name = name;
 	}
-	
+
+	public long getM_id() {
+		return m_id;
+	}
+	public void setM_id(long m_id) {
+		this.m_id = m_id;
+	}
+
+	public List<Long> getPositions() {
+		return positions;
+	}
+	public void setPositions(List<Long> positions) {
+		this.positions = positions;
+	}
 }
