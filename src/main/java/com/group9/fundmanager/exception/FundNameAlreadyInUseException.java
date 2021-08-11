fa29ac3c6ca33a18ce4e8ce1da07dfac5957680f
Abe <934982126@qq.com>
2021-08-11 11:28:14 +0800

    Updated exception folder
    Added two Exception files

---
+++ src/main/java/com/group9/fundmanager/exception/FundNameAlreadyInUseException.java
@@ -0,0 +1,17 @@
+package com.groupnine.fundmanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author abe
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class FundNameAlreadyInUseException extends IllegalArgumentException {
+    private final String name;
+
+    public FundNameAlreadyInUseException(String name) {
+        super("User with email " + name + " already exists.");
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/group9/fundmanager/exception/FundNotFoundException.java
@@ -0,0 +1,17 @@
+package com.groupnine.fundmanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author abe
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class FundNotFoundException extends IllegalArgumentException {
+    private final Long fundId;
+
+    public FundNotFoundException(Long fundId) {
+        super("User with Id " + fundId +  " not found.");
+        this.fundId = fundId;
+    }
+}
