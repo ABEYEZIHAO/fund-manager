176160fa505ade0855008ca6677f2c6beb8e563e
Ritaye <rita-0913@hotmail.com>
2021-08-11 11:50:35 +0800

    Update Manager.java

--- src/main/java/com/group9/fundmanager/pojo/Manager.java
+++ src/main/java/com/group9/fundmanager/pojo/Manager.java
@@ -1,4 +1,55 @@
-package com.group9.fundmanager.pojo;
+package com.dennis.springboot.pojo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "Manager")
 public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "First name")
+    private String firstname;
+
+    @Column(name = "last name")
+    private String lastname;
+
+    @Column(name = "funds")
+    private List<Long> funds;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<Long> getFunds() {
+        return funds;
+    }
+    public void setFunds(List<Long> funds) {
+        this.funds = funds;
+    }
+
 }
